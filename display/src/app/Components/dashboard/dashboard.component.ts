9d9bd72bcb4fcf9850275b0a2cd4e5103a1ba763
i.deshchenya <i.deshchenya@itransition.com>
2023-10-16 21:33:19 +0300

    add dashboard icons

--- display/src/app/Components/dashboard/dashboard.component.ts
+++ display/src/app/Components/dashboard/dashboard.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {
+  eightLineSignals,
+  fiveLineSignals,
+  fourLightSignals,
+  iconsFirstLine,
+  iconsHeadLine,
+  iconsSecondLine,
+  iconsThirdLine, nineLineSignals, sevenLineSignals, sixLineSignals
+} from "./icons-routes";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +15,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-
+  public headLineSignals = iconsHeadLine;  public firstLineSignals = iconsFirstLine;
+  public secondLineSignals = iconsSecondLine; public thirdLineSignals = iconsThirdLine;
+  public fourLineSignals = fourLightSignals; public fiveLineSignals = fiveLineSignals;
+  public sixLineSignals = sixLineSignals; public sevenLineSignals = sevenLineSignals;
+  public eightLineSignals = eightLineSignals; public nineLineSignals = nineLineSignals;
+  constructor() {
+  }
 }
---
+++ display/src/app/Components/dashboard/icons-routes.ts
@@ -0,0 +1,63 @@
+export const iconsHeadLine = [
+  './assets/ico/brake_failure.png', './assets/ico/park_brake.png', './assets/ico/abs.png',
+  './assets/ico/stab_ctrl.png', './assets/ico/engine.png', './assets/ico/turn_left.png',
+  './assets/ico/stop.png', './assets/ico/turn_right.png', './assets/ico/warning.png',
+  './assets/ico/drl.png', './assets/ico/beam_low.png', './assets/ico/beam_high.png',
+  './assets/ico/seat_belt.png'
+];
+
+export const iconsFirstLine = [
+  './assets/ico/asr.png', './assets/ico/halt_brake.png', './assets/ico/pressure_3.png',
+  './assets/ico/worn_brake.png', './assets/ico/ebs.png',
+
+  './assets/ico/pressure_2.png', './assets/ico/ect.png',
+
+  './assets/ico/coolant_heat.png', './assets/ico/heater_1.png', './assets/ico/cab_heater.png',
+  './assets/ico/mirror_heat.png', './assets/ico/window_heat.png',
+]
+
+export const iconsSecondLine = [
+  './assets/ico/engine_fail.png', './assets/ico/oil_pressure.png',
+  './assets/ico/oil_lvl_max.png', './assets/ico/combus_preheat.png',
+
+  './assets/ico/side_light.png', './assets/ico/fog_front.png',
+  './assets/ico/fog_rear.png', './assets/ico/bulb_failure.png',
+]
+
+export const iconsThirdLine = [
+  './assets/ico/eng_inlet_air_filter.png', './assets/ico/engine_coolant_lvl.png', './assets/ico/steering_fluid_lvl.png',
+  './assets/ico/int_illum_1.png', './assets/ico/vent_fan_1.png', './assets/ico/cab_fan.png',
+]
+
+export const fourLightSignals = [
+  './assets/ico/eng_emiss_system_fail.png', './assets/ico/adblue.png',
+  './assets/ico/baby_pram.png', './assets/ico/baby_pram.png'
+]
+
+export const fiveLineSignals = [
+  './assets/ico/wheelchair.png', './assets/ico/immobile.png',
+  './assets/ico/hill_holding.png', './assets/ico/retarder_off.png'
+]
+
+export const sixLineSignals = [
+  './assets/ico/hydraulic_drive.png',
+  './assets/ico/emergency_hammer.png', './assets/ico/extinguisher.png'
+]
+
+export const sevenLineSignals = [
+  './assets/ico/tyre_fail.png', './assets/ico/return_driver.png'
+]
+
+export const eightLineSignals = [
+  './assets/ico/pneumo_1.png', './assets/ico/susp_down.png', './assets/ico/susp_dm.png',
+
+  './assets/ico/transmission_failure.png', './assets/ico/transmission_temperature.png',
+
+  './assets/ico/transmission_convert_temperature.png',
+
+  './assets/ico/battery.png'
+]
+
+export const nineLineSignals = [
+  './assets/ico/pressure_1.png', './assets/ico/diesel.png'
+]
